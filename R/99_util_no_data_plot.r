c7f4e75d501f4c6992463f90f58036e953686e6c
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-07-15 05:40:04 +0200

    scskeleton -> scexample

--- R/99_util_no_data_plot.r
+++ R/99_util_no_data_plot.r
@@ -1,15 +1,18 @@
 # ******************************************************************************
 # ******************************************************************************
 #
-# 05_deliverables.r
+# 99_util_*.r
 #
 # PURPOSE 1:
-#   Set all the deliverables that team members are supposed to manually do/check
-#   every day/week/month.
+#   Utility functions that are used across multiple tasks
 #
 # ******************************************************************************
 # ******************************************************************************
 
-set_deliverables <- function() {
-
+no_data_plot <- function(){
+  data=data.frame(x=0,y=0)
+  q <- ggplot(data=data)
+  q <- q + theme_void()
+  q <- q + annotate("text", label=glue::glue("Ikke noe data {fhi::nb$aa} vise"), x=0, y=0, size=10)
+  q
 }
