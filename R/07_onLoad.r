20b024036844fe0d73828c14d11a55897d48fdaf
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-11-25 05:47:14 +0100

    fix

--- R/07_onLoad.r
+++ R/07_onLoad.r
@@ -14,7 +14,9 @@
 
 .onLoad <- function(libname, pkgname) {
   # Mechanism to authenticate as necessary (e.g. Kerebros)
-  try(system2("/bin/authenticate.sh", stdout = NULL), TRUE)
+  if (file.exists("/bin/authenticate.sh")) {
+    try(system2("/bin/authenticate.sh", stdout = NULL), TRUE)
+  }
 
   # 5_config.r
   set_config()
